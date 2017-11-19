4ee64942fb105e6306060d0194c10b13f8c6052e
森田啓太 <mancheunited7>
2017-11-19 11:11:35 +0900

    add_refernces2

--- db/migrate/20171119005339_create_card_game_experiences.rb
+++ db/migrate/20171119005339_create_card_game_experiences.rb
@@ -4,11 +4,11 @@ class CreateCardGameExperiences < ActiveRecord::Migration[5.1]
       t.integer :card_game_id, null: false
       t.integer :experience_year, null: false
       t.integer :experience_mounth, null:false
-      t.integer :resume_id, null: false
       t.references :resume ,index: true, null: false
+      t.references :card_game, index: true, null: false
 
       t.timestamps
     end
-  add_index :card_game_experiences, [:card_game_id,:resume_id], unique: true
+    add_index :card_game_experiences, [:card_game_id,:resume_id], unique: true
   end
 end
