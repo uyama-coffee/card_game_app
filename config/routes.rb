5036adb2698c2238972c762b4617e0304194ba99
stivan622 <stivan.tr@gmail.com>
2017-11-19 10:00:43 +0900

    rails g scaffold Project image:string title:string content:text shop:references

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :projects
   devise_for :users, path: 'users'
   devise_for :shops, path: 'shops'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119004138) do
+ActiveRecord::Schema.define(version: 20171119005604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "image"
+    t.string "title", null: false
+    t.text "content", null: false
+    t.bigint "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_projects_on_shop_id"
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,4 +63,5 @@ ActiveRecord::Schema.define(version: 20171119004138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "shops"
 end
