001fa63af76758ce241b48e2bd6ec46845dfc30f
森田啓太 <mancheunited7>
2017-11-19 21:52:37 +0900

    modify_devise_screen

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :card_game_experiences
   resources :contacts
   resources :projects
-  devise_for :users, path: 'users'
+  devise_for :users, path: 'users', controllers: {
+    registrations: "users/registrations"
+  }
   devise_for :shops, path: 'shops'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at:"/letter_opener"
+  end
 end
