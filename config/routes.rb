9232773a911c02ca6ee5dfebe88666a5833ca64f
佐々木皓晃 <ssk.hrak123@gmail.com>
2017-11-20 13:23:43 +0900

    fixed mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'mypages/index'
+  resources :mypage, only: [:index]
 
   resources :resumes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119052923) do
+ActiveRecord::Schema.define(version: 20171119013154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,20 +30,6 @@ ActiveRecord::Schema.define(version: 20171119052923) do
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_contacts_on_project_id"
     t.index ["user_id"], name: "index_contacts_on_user_id"
-  end
-
-  create_table "handling_card_games", force: :cascade do |t|
-    t.bigint "project_id", null: false
-    t.bigint "card_game_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["card_game_id"], name: "index_handling_card_games_on_card_game_id"
-    t.index ["project_id"], name: "index_handling_card_games_on_project_id"
-  end
-
-  create_table "mypages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
@@ -110,7 +96,5 @@ ActiveRecord::Schema.define(version: 20171119052923) do
 
   add_foreign_key "contacts", "projects"
   add_foreign_key "contacts", "users"
-  add_foreign_key "handling_card_games", "card_games"
-  add_foreign_key "handling_card_games", "projects"
   add_foreign_key "projects", "shops"
 end
