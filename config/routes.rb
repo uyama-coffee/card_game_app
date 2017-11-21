5fe0f1cd859a0e303ecc54bab420aec22c36cac8
a-kubotera <akira.kubotera+github@gmail.com>
2017-11-21 09:29:21 +0900

    #21 yml追記/エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'top#index'
-  
+
   devise_for :users, path: 'users', controllers: {
     registrations: "users/registrations"
   }
   devise_for :shops, path: 'shops'
-　　　　
   resources :mypage, only: [:index]
   resources :resumes
   resources :card_game_experiences
@@ -14,8 +13,7 @@ Rails.application.routes.draw do
 
   namespace :shop do
     get 'top/index'
-
-  devise_for :shops, path: 'shops'
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at:"/letter_opener"
