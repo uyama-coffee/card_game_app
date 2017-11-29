151846bf596c33cf1c960bd77aa9294765cb9e8f
a-kubotera <akira.kubotera+github@gmail.com>
2017-11-30 00:32:09 +0900

    #36 住所検索機能完成

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,10 +1,10 @@
 class Shop < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   has_many :projects, dependent: :destroy
   has_many :contacts, through: :projects
-
+  has_one :place
   def self.contacts
     Contact.where(project_id: shop_id)
   end
