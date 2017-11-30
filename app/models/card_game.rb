2a2b2fce418c12d072fff2c3241f5d0e568ffb44
a-kubotera <akira.kubotera+github@gmail.com>
2017-11-30 17:31:15 +0900

    #36 取扱カードゲーム検索実装

--- app/models/card_game.rb
+++ app/models/card_game.rb
@@ -1,3 +1,4 @@
 class CardGame < ApplicationRecord
   has_one :card_game_experience
+  has_one :handling_card_game
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ApplicationRecord
   belongs_to :shop, foreign_key: :shop_id
   has_many :contacts, dependent: :destroy
-  #ProjectからPlaceを shop_infoとして呼び出せるようにする。
+  has_many :handling_card_games, dependent: :destroy
+
+  #ProjectからPlaceを[shop_info]として呼び出せるようにする。
   has_one :shop_info, through: :shop, source: :place
+  #Projectから取り扱ってるカードゲームを[card_games]として呼び出せるようにする。
+  has_many :card_games, through: :handling_card_games, source: :card_game
+
   mount_uploader :image, ImageUploader
 
   validates :title, presence: true
