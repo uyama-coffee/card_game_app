c96070e1a81bd2d423be3579778300e39a09a6b3
佐々木皓晃 <ssk.hrak123@gmail.com>
2017-12-08 18:43:51 +0900

    boolean不具合修正

--- app/controllers/shop/shops_controller.rb
+++ app/controllers/shop/shops_controller.rb
@@ -3,12 +3,17 @@ class Shop::ShopsController < ApplicationController
 
   def edit
     @shop = current_shop
+    if @shop.shop_name.blank?
+      @shop.info = false
+    else
+      @shop.info = true
+    end
   end
 
   def update
     @shop = current_shop
     if @shop.update(shop_params)
-      redirect_to edit_shop_shop_path(@shop)
+      redirect_to edit_shop_shop_path(@shop), flash: {check: "check!"}
     else
       render :edit
     end
@@ -18,7 +23,7 @@ class Shop::ShopsController < ApplicationController
 
   def shop_params
     params.require(:shop).permit(
-      :shop_name, :address, :phone_number, :info
+      :shop_name, :address, :phone_number
     )
   end
 
