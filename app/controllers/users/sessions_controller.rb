b2a931280fc2d2a5fe56fdb3722a9fc809c99e27
stivan622 <stivan.tr@gmail.com>
2017-12-11 21:06:27 +0900

    add users sessions controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Users::SessionsController < Devise::SessionsController
+  before_action :check_user_confirmation, only: :create
+
+  private
+
+  def check_user_confirmation
+    user = User.find_by_email(params[:email])
+    redirect_to new_confirmation_path(:user),
+      notice: I18n.t('devise.failure.unconfirmed') unless user && user.confirmed?
+  end
+end
