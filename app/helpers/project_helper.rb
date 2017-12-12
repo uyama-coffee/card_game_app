034fb603fdc4ee3e2bb76ef1008b4b2d18c701ed
a-kubotera <akira.kubotera+github@gmail.com>
2017-12-13 02:21:04 +0900

    #33 レビュー対応

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,10 +1,2 @@
 module ProjectHelper
-  #Shop/projectのフォームでNewかEditかを判定しパスを渡す
-  def choose_new_or_edit
-    if action_name == 'new'
-      shop_projects_path
-    elsif action_name == 'edit'
-      shop_project_path
-    end
-  end
 end
