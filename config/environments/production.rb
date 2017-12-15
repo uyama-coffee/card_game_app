59b78152d3fedb3e4bf5906565bff46a4bfc9d0d
森田啓太 <mancheunited7>
2017-12-15 21:09:33 +0900

    modify_enviroments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: https://rocky-badlands-21526.herokuapp.com/ }
+  config.action_mailer.default_url_options = { host: 'rocky-badlands-21526.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
