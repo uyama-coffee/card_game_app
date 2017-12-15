ec06f38303be5b515aecbc3b45247e041f5cba4b
a-kubotera <akira.kubotera+github@gmail.com>
2017-12-15 16:44:13 +0900

    #27 カードゲーム経験0ヶ月vaidation他

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -10,15 +10,20 @@ class Resume < ApplicationRecord
 
   validates :profile_image,   presence: true
   validates :first_name,      presence: true
-  validates :first_name_kana, presence: true, kana: true 
+  validates :first_name_kana, presence: true, kana: true
   validates :last_name,       presence: true
   validates :last_name_kana,  presence: true, kana: true
   validates :contact_method,  presence: true
-  validates :phone_number,    presence: true, if: :is_phone?
+  validates :phone_number,    presence: true, if: Proc.new { |a| a.contact_method == 1 }
+  validate  :require_career_over_one_month
 
-  def is_phone?
-    contact_method == 1
+  #カードゲーム経験は1ヶ月以上
+  def require_career_over_one_month
+    card_game_experiences.each do |cge|
+      errors.add(:base, :no_experience) if cge.experience_year == 0 && cge.experience_month == 0
+    end
   end
+
 
   extend Enumerize
   enumerize :contact_method, in: { email: 0, phone: 1 }
