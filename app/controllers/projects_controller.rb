8f763fea59d1bbe0aa77e31e1c153e8a5174490a
a-kubotera <akira.kubotera+github@gmail.com>
2017-12-17 14:23:09 +0900

    #21 Map詳細の表示 / ピン移動対応中

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,8 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     #地図情報の作成
     @map = ShopInformation.new.build_map(@project.shop_info)
+    #MAP詳細つきのinfowindowを渡す
+    @map[0][:infowindow]=render_to_string(partial: "/places/infowindow", locals: { place: @project.shop_info })
     #応募機能 インスタンス作成
     @contact = Contact.new
   end
