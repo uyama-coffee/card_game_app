f0d24a020de4c8d12a38a2e29a26282fd72fa3c8
a-kubotera <akira.kubotera+github@gmail.com>
2017-12-18 10:56:11 +0900

    #21 MAP部分のpartial化 / infowindowの移動

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
     #地図情報の作成
     @map = ShopInformation.new.build_map(@project.shop_info)
     #MAP詳細つきのinfowindowを渡す
-    @map[0][:infowindow]=render_to_string(partial: "/places/infowindow", locals: { place: @project.shop_info })
+    @map[0][:infowindow]=render_to_string(partial: "shop/shop_informations/infowindow", locals: { place: @project.shop_info })
     #応募機能 インスタンス作成
     @contact = Contact.new
   end
