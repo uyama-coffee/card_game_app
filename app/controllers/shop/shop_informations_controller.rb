682aa5b77954f732c41b84292de873ca345b5ba9
佐々木皓晃 <ssk.hrak123@gmail.com>
2017-12-20 21:59:36 +0900

    ショップ情報newとcreate作成

--- app/controllers/shop/shop_informations_controller.rb
+++ app/controllers/shop/shop_informations_controller.rb
@@ -1,5 +1,21 @@
 class Shop::ShopInformationsController < ApplicationController
 
+  before_action :duplicate_check, only: [:new, :create]
+  before_action :set_shop_information, only: [:edit, :update]
+
+  def new
+    @shop_information = ShopInformation.new(shop_id: current_shop.id)
+  end
+
+  def create
+    @shop_information = current_shop.build_shop_information(shop_information_params)
+    if @shop_information.save
+      redirect_to edit_shop_shop_information_path(@shop_information), notice: '登録しました'
+    else
+      render :new
+    end
+  end
+
   def edit
     @shop_information = current_shop.shop_information
   end
@@ -7,9 +23,8 @@ class Shop::ShopInformationsController < ApplicationController
   def update
     @shop_information = current_shop.shop_information
     if @shop_information.update(shop_information_params)
-      @flag = false
       @shop_information.save
-      redirect_to edit_shop_shop_information_path(@shop_information)
+      redirect_to edit_shop_shop_information_path(@shop_information), notice: '更新しました'
     else
       render :edit
     end
@@ -17,8 +32,17 @@ class Shop::ShopInformationsController < ApplicationController
 
   private
 
+    def set_shop_information
+      @shop_information = current_shop.shop_information
+      redirect_to new_shop_shop_information_path if @shop_information.nil?
+    end
+
+    def duplicate_check
+      redirect_to edit_shop_shop_information_path(current_shop.shop_information.id) if current_shop.shop_information
+    end
+
     def shop_information_params
       params.require(:shop_information).permit(
-        :shop_id, :shop_name, :description, :address, :latitude, :longitude, :phone_number, :flag)
+        :shop_id, :shop_name, :description, :address, :latitude, :longitude, :phone_number)
     end
 end
