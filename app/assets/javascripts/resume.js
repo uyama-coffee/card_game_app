465c595c665d8843e2add08c0b99ff34508414e9
yuks01 <hiroyuki.aonuma@gmail.com>
2018-02-18 18:45:24 +0900

    resume drop-down click fix

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     $(".add_nested_fields_link").on('click',function(){
       displayChoices();
     });
+    selectedCardGame();
 });
   //連絡方法の切り替え
   function chengeContactMethod(elem){
@@ -24,20 +25,28 @@ $(document).ready(function(){
 
   }
 
+function selectedCardGame(){
+  $(".dropdown-content.select-dropdown li").on('click', function(){
+    $(this).parent().siblings('span').text($(this).text() + "　▼");
+    $(this).parent().siblings('input.card-name').val($(this).val());
+    $(this).parent().removeClass("display-block");
+  });
+}
+
 function displayChoices(){
   var displayed = false;
   $(".card-name").on('click',function(){
-    $("#card-name-choices").addClass("display-block");
+    $($(this).siblings('ul')).addClass("display-block");
     displayed = true;
   });
   // if(displayed)
   $(document).mouseup(function(e) {
-      var container = $("#card-name-choices");
+      var container = $(".dropdown-content.select-dropdown");
       // if the target of the click isn't the container nor a descendant of the container
       if (!container.is(e.target) && container.has(e.target).length === 0) 
       {
         if(displayed){
-            $("#card-name-choices").removeClass("display-block");
+            $(".dropdown-content.select-dropdown").removeClass("display-block");
         } else {
 
         }
